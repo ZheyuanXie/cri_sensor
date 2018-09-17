1b83e1ddd788f11c8bd2167455e92fd4424bce95
zheyuanxie <zyxie.sjtu@gmail.com>
2018-09-18 03:42:52 +0800

    publish data using rosserial

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,44 @@
 #include <mbed.h>
 #include <MLX90363.h>
+#include <ros.h>
+#include <std_msgs/Int16.h>
 
 SPI spi(D11, D12, D13);
-Serial serial(USBTX, USBRX);  // tx, rx
-MLX90363 mlx90363_1(D10, &spi);
-MLX90363 mlx90363_2(D9, &spi);
+// Serial serial(USBTX, USBRX);
+MLX90363 sensor1(D10, &spi);
+MLX90363 sensor2(D9, &spi);
+MLX90363 sensor3(D8, &spi);
+
+ros::NodeHandle nh;
+std_msgs::Int16 int16_msg;
+ros::Publisher pub1("sensor1", &int16_msg);
+ros::Publisher pub2("sensor2", &int16_msg);
+ros::Publisher pub3("sensor3", &int16_msg);
 
 int main() {
+    nh.initNode();
+    nh.advertise(pub1);
+    nh.advertise(pub2);
+    nh.advertise(pub3);
     spi.format(8,1);
     spi.frequency(500000);
-    serial.baud(9600);
+    // serial.baud(9600);
     wait(1);
 
     while(1) {
-        if (mlx90363_1.Poll()) serial.printf("crc error 1\n");
-        wait_ms(1);
-        if (mlx90363_2.Poll()) serial.printf("crc error 2\n");
-        serial.printf("Angel_1: %" PRId16 " Angel_2: %" PRId16 "\n",mlx90363_1.Get_Angel(),mlx90363_2.Get_Angel());
-        wait_ms(1);
+        sensor1.Poll();
+        int16_t angle1 = sensor1.Get_Angel();
+        sensor2.Poll();
+        int16_t angle2 = sensor2.Get_Angel();
+        sensor3.Poll();
+        int16_t angle3 = sensor3.Get_Angel();
+        int16_msg.data = angle1;
+        pub1.publish(&int16_msg);
+        int16_msg.data = angle2;
+        pub2.publish(&int16_msg);
+        int16_msg.data = angle3;
+        pub3.publish(&int16_msg);
+        nh.spinOnce();
+        wait_ms(10);
     }
 }
